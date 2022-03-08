4c59ccdf0f429fd3abda7123ecc037c5e7623afd
Daniel McNeill <danieljmcneill7@gmail.com>
2022-03-08 14:16:52 -0500

    Pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+product.create([
+{
+    title: "Candy Bar",
+    price: "$1.00",
+    description: "Any candy of your choosing falls under this category.",
+    image: "candy.png"
+},
+{
+    title: "Ramen",
+    price: "$0.25",
+    description: "Make sure to keep the microwave clean!",
+    image: "ramen.jpg"
+},
+{
+    title: "Soda Cans",
+    price: "$0.75",
+    description: "Nice and refreshing!",
+    image: "soda.png"
+},
+{
+    title: "Sweet Tea",
+    price: "$1.25",
+    description: "For the Southerns in the Company!",
+    image: "tea.png"
+}
+])
